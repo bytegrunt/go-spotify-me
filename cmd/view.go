f7489530137d6d8b59612b226567431c1fcdad1f
bytegrunt <207869117+bytegrunt@users.noreply.github.com>
2025-04-20 16:35:08 -0400

    remove unused aong and artist methods

--- cmd/view.go
+++ cmd/view.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"strings"
 )
 
 func (m appModel) View() string {
@@ -45,66 +44,4 @@ func (m appModel) renderMenu() string {
 		m.me.Country,
 		m.me.ProfileURL,
 	)
-}
-
-func (m appModel) renderArtists() string {
-	var s strings.Builder
-
-	// Dynamic column widths
-	totalWidth := m.windowSize.Width
-	if totalWidth == 0 {
-		totalWidth = 100 // fallback
-	}
-	colName := 30
-	colGenre := totalWidth - colName - 12 - 5 // leave space for padding and popularity
-	colPop := 5
-
-	s.WriteString(fmt.Sprintf("%s %s %s\n",
-		truncateOrPad("Name", colName),
-		truncateOrPad("Genres", colGenre),
-		truncateOrPad("Popularity", colPop),
-	))
-	s.WriteString(strings.Repeat("-", totalWidth) + "\n")
-
-	for _, artist := range m.artists.Artists {
-		s.WriteString(fmt.Sprintf("%s %s %d\n",
-			truncateOrPad(artist.Name, colName),
-			truncateOrPad(artist.Genres, colGenre),
-			artist.Popularity,
-		))
-	}
-	s.WriteString("\n[←] previous, [→] next, [q] back to menu\n")
-	return s.String()
-}
-
-func (m appModel) renderSongs() string {
-	var s strings.Builder
-
-	totalWidth := m.windowSize.Width
-	if totalWidth == 0 {
-		totalWidth = 100
-	}
-	colName := 40
-	colArtist := 30
-	colAlbum := totalWidth - colName - colArtist - 12 - 5
-	colPop := 5
-
-	s.WriteString(fmt.Sprintf("%s %s %s %s\n",
-		truncateOrPad("Name", colName),
-		truncateOrPad("Artist", colArtist),
-		truncateOrPad("Album", colAlbum),
-		truncateOrPad("Popularity", colPop),
-	))
-	s.WriteString(strings.Repeat("-", totalWidth) + "\n")
-
-	for _, song := range m.songs.Songs {
-		s.WriteString(fmt.Sprintf("%s %s %s %d\n",
-			truncateOrPad(song.Name, colName),
-			truncateOrPad(song.Artist, colArtist),
-			truncateOrPad(song.Album, colAlbum),
-			song.Popularity,
-		))
-	}
-	s.WriteString("\n[←] previous, [→] next, [q] back to menu\n")
-	return s.String()
 }
